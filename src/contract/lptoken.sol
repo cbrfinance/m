b472d09dda4377bc7418bb35f97dba60e267a205
cobrafinance.contact <cobrafinance.contact@gmail.com>
2022-04-16 23:46:38 -0400

    addwallet

--- src/contract/lptoken.sol
+++ src/contract/lptoken.sol
@@ -10,16 +10,20 @@ contract Calculator{
     uint reseveredETH = 23282592115161903556371;
     uint reseveredUSD = 71537139645692000000000000;
     uint totalSupply = 649077916377283094;
+    uint lpamout = 123412;
     uint sqrtK = HomoraMath.sqrt(reseveredETH*reseveredUSD).fdiv(totalSupply);
 
     uint priceofEth = reseveredUSD*(2**112)/reseveredETH;
     uint priceofUSD = 2**112;
 
+
     uint lptokenPrice = (2*sqrtK*HomoraMath.sqrt(priceofEth)/2**56*HomoraMath.sqrt(priceofUSD)/2**56);
 
-    function getLPTOKEN() view public returns(uint, uint, uint) {
+    function getLPTOKEN() view public returns(uint, uint, uint, uint) {
         
-        return (sqrtK, priceofEth, lptokenPrice/2**112);
+        return (sqrtK, priceofEth, lptokenPrice*totalSupply/2**112, lptokenPrice*lpamout*reseveredETH/reseveredUSD/2**112);  //that give us USD in 10^^18
     }
+
+
     
 }
