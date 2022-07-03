0555abc72a5944de9c8633b4899c3b687f6ef800
cobrafinance.contact <cobrafinance.contact@gmail.com>
2022-07-03 02:54:15 -0400

    added bond

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -121,15 +121,15 @@ export const Provider = ({ children }) => {
             method: "wallet_addEthereumChain",
             params: [
               {
-                  chainId: '0x3E9',
-                  chainName: 'Klaytn Baobob',
-                  rpcUrls: ["https://api.baobab.klaytn.net:8651"],
+                  chainId: '0x2019',
+                  chainName: 'Klaytn Cypress',
+                  rpcUrls: ["https://public-node-api.klaytnapi.com/v1/cypress"],
                   nativeCurrency: {
                       name: 'KLAY',
                       symbol: 'KLAY',
                       decimals: 18
                   },
-                  blockExplorerUrls: ['https://baobab.scope.klaytn.com/']
+                  blockExplorerUrls: ['https://scope.klaytn.com/']
               }
           ],
           });
