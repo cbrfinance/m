500780f96375c2e112d6efcc61b3da5558eaf22f
cobrafinance.contact <cobrafinance.contact@gmail.com>
2022-07-02 22:43:50 -0400

    added bond

--- src/components/Governance.jsx
+++ src/components/Governance.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 function Governance() {
 
-    const [first, setFirst] = useState(false);
+
 
     return(
         <div className="p-1 flex space-y-4 flex-col">
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React  from 'react';
-import { Link, NavLink, useHref} from 'react-router-dom'
+import { NavLink} from 'react-router-dom'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import{faBolt, faBook, faRightLeft, faCircleUser} from "@fortawesome/free-solid-svg-icons";
 import {faChartBar, faClone, faGem} from "@fortawesome/free-regular-svg-icons";
