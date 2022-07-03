e3efdc10382b0430a6cae154e849dc7abba404cc
cobrafinance.contact <cobrafinance.contact@gmail.com>
2022-07-02 22:37:55 -0400

    added bond

--- src/components/Zap.jsx
+++ src/components/Zap.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Zap = () => {
     return (
-        <div>
-            asd
+        <div className="p-1 flex space-y-4 flex-col">
+            <div className='w-full p-4 bg-gray-100 rounded-lg max-w-3xl h-10 m-auto'>
+                
+            </div>
         </div>
     )
 }
